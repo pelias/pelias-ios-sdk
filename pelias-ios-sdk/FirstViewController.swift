a6198d3e0bb9d0ea3e148906a63c67ae9cb4c379
Matt Smollinger <matt.smollinger@mapzen.com>
2015-11-30 09:49:31 -0500

    Initial Commit

---
+++ pelias-ios-sdk/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  pelias-ios-sdk
+//
+//  Created by Matt on 11/30/15.
+//  Copyright © 2015 Mapzen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
