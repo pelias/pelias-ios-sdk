59dd5f48fa90c3af98b9cda7ec184fdc10429d30
Matt Smollinger <matt.smollinger@mapzen.com>
2016-02-03 09:49:35 -0500

    Add Swift Package Manager Support
    Update the Changelog as well

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  pelias-ios-sdk
+//
+//  Created by Matt on 2/3/16.
+//  Copyright © 2016 Mapzen. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Pelias"
+)
